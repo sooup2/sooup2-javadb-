2be43ee5a3979da144999b697a34cb10b0f26519
kimbomi <rlaqhal719@gmail.com>
2018-03-29 16:49:15 +0900

    Rect2 가로길이 세로길이 좌표로 사각형 나타내는 코드 추가

--- javasutdy2/src/javasutdy2/example/Main.java
+++ javasutdy2/src/javasutdy2/example/Main.java
@@ -3,7 +3,14 @@ package javasutdy2.example;
 public class Main {
 
 	public static void main(String[] args) {
-		Rect.printXY();
+		
+		Rect1 rect1 = new Rect1();
+		Rect2 rect2 = new Rect2();
+		
+		System.out.print("1. ");
+		rect1.printXY();
+		System.out.print("2. ");
+		rect2.printXYAD();
 	}
 
 }
@@ -18,8 +25,8 @@ class Point{
 
 	public void SetXY(int x, int y)
 	{
-		this.x=x;
-		this.y=y;
+		this.setX(x);
+		this.setY(y);
 	}
 	public Point ()
 	{
@@ -28,11 +35,11 @@ class Point{
 	}
 }
 
-class Rect{
+class Rect1{
 	
 	static Point spot = new Point();
 	
-	public static void printXY()
+	public void printXY()
 	{
 		System.out.print("사각형을 이루는 4개의 점의 좌표는 ");
 		spot.SetXY(0,0);
@@ -42,6 +49,35 @@ class Rect{
 		spot.SetXY(0,3);
 		System.out.print("("+spot.getX()+","+spot.getY()+"),");
 		spot.SetXY(3,3);
-		System.out.print("("+spot.getX()+","+spot.getY()+")입니다.");
+		System.out.println("("+spot.getX()+","+spot.getY()+")입니다.");
 	}
+}
+
+class Rect2{
+	
+	static Point spot = new Point();
+	private double across;
+	private double down;
+		
+	public double getAcross() {return across;}
+	public void setAcross(double across) {this.across = across;}
+	public double getDown() {return down;}
+	public void setDown(double down) {this.down = down;}
+
+	public void printXYAD()
+	{
+		spot.SetXY(0,0);
+		across=3.0;
+		down=3.0;
+		System.out.print("사각형은 좌표 ");
+		System.out.print("("+spot.getX()+","+spot.getY()+")을 기준으로 ");
+		System.out.print("가로길이 " + across+", ");
+		System.out.println("세로길이 " + down + "입니다.");
+	}
+}
+
+class Rect3{
+	
+	private Point leftUp;
+	private Point RightDown;
 }
